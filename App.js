e7789d573909ba9aacb0de79db9cead4dca83794
prafful0026 <prafful0026@gmail.com>
2021-04-05 23:58:31 +0530

    basic setup done

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { Button,StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  
+  const[name,setName]=useState("praful");
+   const buttonHandler=()=>{
+     setName("dwcbjkwg")
+   }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+       <Text style={styles.header}>huidcwegh</Text> 
+      </View>
+      <Text>{name}</Text>
+      <Text>YO YO</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="button" onPress={buttonHandler}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +31,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    color:"pink",
+    backgroundColor:"green",
+  },
+  buttonContainer:{
+    marginTop:20,
+     
+  }
 });
